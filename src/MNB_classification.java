0f9e82a1343e54e3b7d9bfeca7afe4426bdd73c5
Daniel Haskin <djhaskin987@gmail.com>
2013-04-26 00:10:00 -0600

    whodj

--- src/MNB_classification.java
+++ src/MNB_classification.java
@@ -4,20 +4,96 @@ import java.util.*;
 
 public class MNB_classification
 {
+    public static double TRAINING_RATIO = .8;
+
     Map<String,Pair<String,Map<String, Integer>>>
-        RawDocumentCollection;
+        DC;
+    Map<String,Pair<String,Map<String, Integer>>>
+        DC_test;
+    Map<String,Pair<String,Map<String, Integer>>>
+        DC_training;
+    Map<String,Pair<String,Map<String, Integer>>>
+        DC_test;
+    Map<String,Pair<String,Map<String, Integer>>>
+        DC_training;
+    Set<String> Classifications;
+    Set<String> Vocabulary;
 
     public MNB_classification(String CorpusDirName, String StopwordsFile)
     {
         Map<String,List<String>> CorpusTokens =
             new HashMap<String,List<String>>();
+        Classifications = new HashSet<String>();
+        Vocabulary = new HashSet<String>();
         StopWords sw = new StopWords(StopwordsFile);
         LoadCorpusTokens(CorpusTokens, CorpusDirName, sw);
         // ID -> (Class,(Tokens -> Count))
-        RawDocumentCollection = ClassifyTokens(CorpusTokens, CorpusDirName);
+        DC = ClassifyTokens(CorpusTokens, CorpusDirName);
+        DC_test =
+            new HashMap<String,Pair<String,Map<String,Integer>>>();
+        DC_training =
+            new HashMap<String,Pair<String,Map<String,Integer>>>();
+        LoadTrainingAndTest(DC,
+                DC_training,
+                DC_test);
     }
 
-    Map<String, Pair<String, Map<String, Integer>>>
+    private void LoadTrainingAndTest(
+        Map<String,Pair<String,Map<String, Integer>>>
+            DC,
+        Map<String,Pair<String,Map<String, Integer>>>
+            DC_training,
+        Map<String,Pair<String,Map<String, Integer>>>
+            DC_test)
+    {
+        Random rng = new Random();
+        double dice;
+
+        // Choose random K algorithm --
+        // Note how many documents I will sift through to create the training
+        // set.
+        int RestOfTheDocuments = DC.size();
+
+        // Find out how many documents I need in the training set ("K").
+        int LeftTrainingToPick = (int)(TRAINING_RATIO * denominator);
+
+        // Guard against divide by zero
+        if (RestOfTheDocuments == 0)
+            return;
+
+        for (Map.Entry<String,Pair<String,Map<String,Integer>>>
+                Doc : DC.entrySet())
+        {
+            double ratio = ((double)LeftTrainingToPick) /
+                ((double)RestOfTheDocuments);
+            dice = rng.nextFloat();
+            if (dice < ratio)
+            // We pick a document for the training set in this case.
+            // decrease the number of documents we still need to find (numerator)
+            // and decrease the number of documents we still need to sift
+            // through (denominator).
+            {
+                DC_training.put(
+                        Doc.getKey(),
+                        Doc.getValue());
+                LeftTrainingToPick--;
+                RestOfTheDocuments--;
+            }
+            else
+            {
+            // We pick a document for NOT the training set (the test set)
+            // decrease the number of documents we still need to sift through
+            // to find the correct amount of training set documents.
+                RestOfTheDocuments--;
+                DC_test.put(
+                        Doc.getKey(),
+                        Doc.getValue());
+            }
+        }
+
+    }
+
+    private Map<String, Pair<String, Map<String, Integer>>>
         ClassifyTokens(Map<String, List<String>> CorpusTokens,
                 String CorpusDirName)
     {
@@ -35,6 +111,7 @@ public class MNB_classification
                 FileNameSansBase.replaceFirst("/[^/]*$","");
             String Class =
                 RawClass.replace('/','.');
+            Classifications.add(Class);
 
             String BaseFileName =
                 FileNameSansBase.replaceAll("[^/]*/","");
@@ -45,6 +122,7 @@ public class MNB_classification
 
             for (String token : FileTokens.getValue())
             {
+                Vocabulary.add(token);
                 Integer count = WordCounts.get(token);
                 if (count == null)
                 {
@@ -102,8 +180,14 @@ public class MNB_classification
     public static void main(String args[])
     {
         MNB_classification c =
-            new MNB_classification("../test", "../data/stopwords");
+            new MNB_classification("../test/s9test", "../data/stopwords");
         System.out.println("Tokens:");
-        System.out.println(c.RawDocumentCollection);
+        System.out.println(c.DC);
+        System.out.println("TRAINING");
+        System.out.println(c.DC_training);
+        System.out.println("TEST");
+        System.out.println(c.DC_test);
+        System.out.println("CLASSES");
+        System.out.println(c.Classifications);
     }
 }
